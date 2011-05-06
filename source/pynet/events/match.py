b49e7855e568accc37d68ff6ab03dd0b75132774
Lisa Glendenning <lglenden@cs.washington.edu>
2011-05-06 14:06:23 -0700

    bugfix

--- source/pynet/events/match.py
+++ source/pynet/events/match.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 
 import itertools
+import operator
 
 #############################################################################
 #############################################################################
@@ -19,11 +20,11 @@ class Match(tuple):
 
 MatchAny = Match()
 
-class MatchOperator(tuple):
+class MatchOperator(Match):
     
     def __new__(cls, operator, *matchers):
-        self = super(Match, cls).__new__(cls, operator, *matchers)
-        self.operator = operator
+        self = super(MatchOperator, cls).__new__(cls, operator, *matchers)
+        self.operator = self[0]
         self.matchers = self[1:]
         return self
     
@@ -31,24 +32,24 @@ class MatchOperator(tuple):
         operator = self.operator
         matchers = self.matchers
         return reduce(operator, itertools.imap(lambda m: m & value, matchers))
+
+And = operator.__and__
+Or = operator.__or__
     
 class MatchType(Match):
     
-    def __new__(cls, type, value):
-        self = super(MatchType, cls).__new__(cls, type, value)
-        self.type = type
-        self.value = value
+    def __new__(cls, type,):
+        self = super(MatchType, cls).__new__(cls, type,)
+        self.type = self[0]
         return self
     
     def __and__(self, value):
-        if not isinstance(value, self.type):
-            return False
-        return self.value & value
+        return isinstance(value, self.type)
 
 class MatchPredicate(Match):
 
-    def __new__(cls, values):
-        self = super(MatchPredicate, cls).__new__(cls, *values)
+    def __new__(cls, predicate):
+        self = super(MatchPredicate, cls).__new__(cls, predicate,)
         self.predicate = self[0]
         return self
     
