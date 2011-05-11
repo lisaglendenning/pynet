c7c825d963642b86cdaf13ec4c9dbbb3f02df4fc
Lisa Glendenning <lglenden@cs.washington.edu>
2011-05-11 09:45:00 -0700

    backporting

--- source/pynet/io/poll/ipolls.py
+++ source/pynet/io/poll/ipolls.py
@@ -1,8 +1,13 @@
 # @copyright
 # @license
 
-import abc
-import collections
+import sys
+
+if sys.version_info[1] < 6:
+    import UserDict
+else:
+    import abc
+    import collections
 
 #############################################################################
 #############################################################################
@@ -14,49 +19,94 @@ POLLIN, POLLOUT, POLLHUP, POLLEX, = EVENTS
 #############################################################################
 #############################################################################
 
-class IPoller(collections.MutableMapping):
-    r"""
-    Registered objects must either be integer file descriptors, or
-    be hashable objects with a fileno() method that returns a file descriptor.
-    """
-    __metaclass__ = abc.ABCMeta
-
-    @classmethod
-    def get_fileno(cls, obj):
-        if isinstance(obj, int):
-            return obj
-        fd = obj.fileno()
-        if not isinstance(fd, int):
-            raise TypeError(obj)
-        return fd
+if sys.version_info[1] < 6:
     
-    def __init__(self):
-        self.registry = {}
-
-    def __getitem__(self, fd):
-        return self.registry[fd]
-
-    def __setitem__(self, fd, events):
-        self.registry[fd] = events
-        
-    def __delitem__(self, fd):
-        if fd not in self:
-            raise KeyError(fd)
-        del self.registry[fd]
-    
-    def __len__(self):
-        return len(self.registry)
-    
-    def __iter__(self):
-        for k in self.registry:
-            yield k
-
-    @abc.abstractmethod
-    def poll(self, timeout=0.0):
-        r"""
-        timeout: seconds (float)
+    class IPoller(UserDict.DictMixin):
         """
-        pass
+        Registered objects must either be integer file descriptors, or
+        be hashable objects with a fileno() method that returns a file descriptor.
+        """
+    
+        @classmethod
+        def get_fileno(cls, obj):
+            if isinstance(obj, int):
+                return obj
+            fd = obj.fileno()
+            if not isinstance(fd, int):
+                raise TypeError(obj)
+            return fd
+        
+        def __init__(self):
+            self.registry = {}
+    
+        def __getitem__(self, fd):
+            return self.registry[fd]
+    
+        def __setitem__(self, fd, events):
+            self.registry[fd] = events
+            
+        def __delitem__(self, fd):
+            if fd not in self:
+                raise KeyError(fd)
+            del self.registry[fd]
+        
+        def __len__(self):
+            return len(self.registry)
+        
+        def __iter__(self):
+            for k in self.registry:
+                yield k
+
+        def poll(self, timeout=0.0):
+            r"""
+            timeout: seconds (float)
+            """
+            pass
+else:
+    
+    class IPoller(collections.MutableMapping):
+        r"""
+        Registered objects must either be integer file descriptors, or
+        be hashable objects with a fileno() method that returns a file descriptor.
+        """
+        __metaclass__ = abc.ABCMeta
+    
+        @classmethod
+        def get_fileno(cls, obj):
+            if isinstance(obj, int):
+                return obj
+            fd = obj.fileno()
+            if not isinstance(fd, int):
+                raise TypeError(obj)
+            return fd
+        
+        def __init__(self):
+            self.registry = {}
+    
+        def __getitem__(self, fd):
+            return self.registry[fd]
+    
+        def __setitem__(self, fd, events):
+            self.registry[fd] = events
+            
+        def __delitem__(self, fd):
+            if fd not in self:
+                raise KeyError(fd)
+            del self.registry[fd]
+        
+        def __len__(self):
+            return len(self.registry)
+        
+        def __iter__(self):
+            for k in self.registry:
+                yield k
+    
+        @abc.abstractmethod
+        def poll(self, timeout=0.0):
+            r"""
+            timeout: seconds (float)
+            """
+            pass
 
 ##############################################################################
 ##############################################################################
